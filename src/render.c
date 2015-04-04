ecaaeb3fbce9365407b68e3ed95ad590d03d4485
Max Yankov <golergka@Aperture-Science-Mac.local>
2015-04-04 18:31:19 +0300

    Rendering screen border

--- src/render.c
+++ src/render.c
@@ -1,11 +1,35 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
 #include "render.h"
 
-void render()
+void clear_screen()
 {
 	rewind(stdout);
 	printf("\033[2J\033[1;1H");
-	puts("Render frame");
-	usleep(100);
+}
+
+void render_line(int c, int length)
+{
+	for(int x = 0; x < length; x++)
+	{
+		putchar(c);
+	}
+}
+
+void render()
+{
+	clear_screen();
+
+	struct winsize w;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+	render_line('+', w.ws_col);
+	for(int y = 1; y < w.ws_row - 1; y++)
+	{
+		putchar('+');
+		render_line(' ', w.ws_col - 2);
+		putchar('+');
+	}
+	render_line('+', w.ws_col);
+	usleep(1);
 }
